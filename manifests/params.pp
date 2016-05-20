f2124d7b89b407cf5c83780170eb3ef99d23743b
Mark Ranallo <mranallo@gmail.com>
2016-05-19 21:29:28 -0700

    Define the version in a local variable

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,6 @@
 class consul_replicate::params {
-
+  $version = '0.2.0'
+  
   case $::architecture {
     'x86_64', 'amd64': { $arch = 'amd64' }
     'i386':            { $arch = '386'   }
@@ -9,7 +10,7 @@ class consul_replicate::params {
   case $::operatingsystem {
     ubuntu: {
       $bin_dir      = '/usr/local/bin'
-      $download_url = "https://releases.hashicorp.com/consul-replicate/${consul_replicate::version}/consul-replicate_linux_${arch}.zip"
+      $download_url = "https://releases.hashicorp.com/consul-replicate/${version}/consul-replicate_linux_${arch}.zip"
     }
     default: { fail("Unsupported operating system: ${::operatingsystem}") }
   }
